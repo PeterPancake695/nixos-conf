97e1a9ce0318a2dc74187b7751359748457c0e64
PeterPancake695 <petrupaun55@gmail.com>
2025-07-26 23:33:00 +0300

    jamaican graveyard

--- nixos/modules/wm.nix
+++ nixos/modules/wm.nix
@@ -10,9 +10,11 @@
   };
 
   security.pam.services.hyprlock = {};
+  security.pam.services.gdm.enableGnomeKeyring = true;
 
   services = {
     displayManager.gdm.enable = true;
+    gnome.gnome-keyring.enable = true;
 
     libinput.enable = true;
 
@@ -22,6 +24,19 @@
 
     libinput.touchpad = {
       middleEmulation = true;
+    };
+  };
+
+  systemd.user.services.gnome-keyring-daemon = {
+    enable = true;
+    description = "GNOME Keyring Daemon";
+    wantedBy = ["default.target"];
+
+    serviceConfig = {
+      Type = "dbus";
+      BusName = "org.gnome.keyring";
+      ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --foreground --components=pkcs11,secrets,ssh,gpg";
+      Environment = "SSH_AUTH_SOCK=%t/keyring/ssh";
     };
   };
 }
