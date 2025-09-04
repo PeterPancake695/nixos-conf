880d7ed0544b35136aaed4e5d9f46f1cc9bbac9f
PeterPancake695 <petrupaun55@gmail.com>
2025-09-04 13:11:44 +0300

    fix

--- nixos/modules/wm.nix
+++ nixos/modules/wm.nix
@@ -2,11 +2,6 @@
 {
   programs.hyprland.enable = true;
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  };
-
   services.displayManager.gdm.enable = true;
   security.pam.services.hyprlock = { };
   hardware.opentabletdriver.enable = true;
